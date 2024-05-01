bb2e45425ad4fca3c240faf4f731338f47aeccc8
Liborio Daniele Frasca <frascaliboriodaniele@gmail.com>
2024-05-01 03:23:38 +0200

    add img.ico, logo and mod hero

--- src/component/home/HeroComponent.jsx
+++ src/component/home/HeroComponent.jsx
@@ -1,4 +1,5 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Image } from "react-bootstrap";
+import logoNutribal from "../../assets/logo-nutri-bal.png";
 
 function HeroComponet() {
   return (
@@ -9,14 +10,12 @@ function HeroComponet() {
       >
         <Row>
           <Col className="d-flex flex-column align-items-center justify-content-center">
-            <h1>NutriBal</h1>
-            <h4>L&apos;app per la tua salute!</h4>
-            <p className="text-center">
-              Siamo entusiasti di darti il benvenuto nella nostra applicazione
-              dedicata alla salute e alla nutrizione. Qui troverai tutte le
-              risorse di cui hai bisogno per raggiungere i tuoi obiettivi di
-              benessere e alimentazione in modo sano e sostenibile.
-            </p>
+            <Image
+              src={logoNutribal}
+              alt="logo-nutribal"
+              width={400}
+              height={400}
+            />
           </Col>
         </Row>
       </Container>
